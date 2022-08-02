259364fe171f976c04936b91fd3c67f8adf784bf
JoseAMoralesF <109744936+JoseAMoralesF@users.noreply.github.com>
2022-08-01 21:42:48 -0600

    Add files via upload

--- Hunter.java
+++ Hunter.java
@@ -36,7 +36,9 @@ public class Hunter extends Actor
     public void eat()
     {
         Actor food;
+        Actor food2;
         food = getOneObjectAtOffset(0, 0, Food.class);
+        food2 = getOneObjectAtOffset(0, 0, Food2.class);
         int comidos = 0;
         if (food != null)
         {
@@ -55,12 +57,37 @@ public class Hunter extends Actor
             }
             else
             {
-                world.addObject(newFood2(), Greenfoot.getRandomNumber(world.getWidth() - 5), Greenfoot.getRandomNumber(world.getHeight() - 5));
+                world.addObject(new Food2(), Greenfoot.getRandomNumber(world.getWidth() - 5), Greenfoot.getRandomNumber(world.getHeight() - 5));
             }
             if (Greenfoot.getRandomNumber(3) == 1)
                 {
                     world.addObject(new Lion(), 0, Greenfoot.getRandomNumber(world.getHeight() - 5));
                 }
+        }
+        
+        if (food2 != null)
+        {
+            World world;
+            world = getWorld();
+            world.removeObject(food);
+            Greenfoot.playSound("yum.wav");
+            comidos++;
+            if (comidos > 10)
+                {
+                    new GreenfootImage(100, 100).drawString("Juego Ganado", 10 , 10);
+                }
+            if (Greenfoot.getRandomNumber(2) == 1)
+            {
+                world.addObject(new Food(), Greenfoot.getRandomNumber(world.getWidth() - 5), Greenfoot.getRandomNumber(world.getHeight() - 5));   
+            }
+            else
+            {
+                world.addObject(new Food2(), Greenfoot.getRandomNumber(world.getWidth() - 5), Greenfoot.getRandomNumber(world.getHeight() - 5));
+            }
+            //if (Greenfoot.getRandomNumber(3) == 1)
+            //    {
+            //        world.addObject(new Monkey(), 0, Greenfoot.getRandomNumber(world.getHeight() - 5));
+            //   }
         }
     }
 }
